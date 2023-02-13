0274ae5e420dc14ec407e9e42fa4b09ba28e7806
Marcus Little <mcolelittle@gmail.com>
2023-02-13 15:57:41 -0500

    test proxy

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,11 +8,16 @@
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
 exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
+  const { createPage, createRedirect } = actions
   createPage({
     path: "/using-dsg",
     component: require.resolve("./src/templates/using-dsg.js"),
     context: {},
     defer: true,
+  })
+    createRedirect({
+    fromPath: `/page-2/`,
+    toPath: `https://www.google.com/`,
+    statusCode: 200,
   })
 }
